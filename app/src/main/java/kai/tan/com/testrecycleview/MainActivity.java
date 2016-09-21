6ae4db6dea2bf0eae65e1992846511757fdeb252
luobodidi <1067629251@qq.com>
2016-09-21 18:05:49 +0800

    2016年9月21日18:05:42

--- app/src/main/java/kai/tan/com/testrecycleview/MainActivity.java
+++ app/src/main/java/kai/tan/com/testrecycleview/MainActivity.java
@@ -1,13 +1,72 @@
 package kai.tan.com.testrecycleview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "tag";
+    private ArrayList<String> mData;
+    private RecyclerView mRecyclerView;
+    private HomeAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initData();
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        //        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        mRecyclerView.setLayoutManager(new GridLayoutManager(this, 4));
+        mRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(4, StaggeredGridLayoutManager.VERTICAL));
+        mRecyclerView.setAdapter(mAdapter = new HomeAdapter());
+        mRecyclerView.addItemDecoration(new DividerGridItemDecoration(this));
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+    }
+
+    private void initData() {
+
+        mData = new ArrayList<>();
+        for (int i = 'A'; i < 'z'; i++) {
+            mData.add("" + i);
+        }
+    }
+
+    class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.MyViewHolder> {
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            MyViewHolder holder = new MyViewHolder(LayoutInflater.from(MainActivity.this).inflate(R.layout.item_1, parent, false));
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+            holder.tv.setText(mData.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return mData.size();
+        }
+
+        class MyViewHolder extends RecyclerView.ViewHolder {
+
+            TextView tv;
+
+            public MyViewHolder(View view) {
+                super(view);
+                tv = (TextView) view.findViewById(R.id.id_num);
+            }
+        }
     }
 }
